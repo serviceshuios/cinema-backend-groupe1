04c621e8462ec5b711dd59b7174e3c32c861a264
Antoine Limouzin <limouzin.antoine@gmail.com>
2020-01-13 10:53:16 +0100

    Ajout services (erreurs sur des imports non existants)

---
+++ src/main/java/com/montparnasse/cinema/service/ProjectionService.java
@@ -0,0 +1,40 @@
+package com.montparnasse.cinema.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class ProjectionService {
+	
+	@Autowired
+	private ProjectionDao dao;
+	
+	public Projection add(Projection e)
+	{
+		return dao.save(e);
+	}
+	
+	public Projection update(Projection e)
+	{
+		return dao.save(e);
+	}
+	
+	public boolean delete(Long id)
+	{
+		dao.deleteById(id);
+		return true;
+	}
+	
+	public Projection get(Long id)
+	{
+		return dao.findById(id).get();
+	}
+	
+	public List<Projection> getAll()
+	{
+		return dao.findAll();
+	}
+}
---
+++ src/main/java/com/montparnasse/cinema/service/SalleService.java
@@ -0,0 +1,40 @@
+package com.montparnasse.cinema.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class SalleService {
+	
+	@Autowired
+	private SalleDao dao;
+	
+	public Salle add(Salle e)
+	{
+		return dao.save(e);
+	}
+	
+	public Salle update(Salle e)
+	{
+		return dao.save(e);
+	}
+	
+	public boolean delete(Long id)
+	{
+		dao.deleteById(id);
+		return true;
+	}
+	
+	public Salle get(Long id)
+	{
+		return dao.findById(id).get();
+	}
+	
+	public List<Salle> getAll()
+	{
+		return dao.findAll();
+	}
+}
