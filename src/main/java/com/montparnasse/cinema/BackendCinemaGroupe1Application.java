dbf1a40f7203e6909c2102d6fc5e2604ed3107b3
HUIOS <services.huios@gmail.com>
2020-01-13 10:20:09 +0100

    commit initial

---
+++ src/main/java/com/montparnasse/cinema/BackendCinemaGroupe1Application.java
@@ -0,0 +1,13 @@
+package com.montparnasse.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCinemaGroupe1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCinemaGroupe1Application.class, args);
+	}
+
+}
